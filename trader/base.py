18eb48432ac892f3e43bf60b7f13d3d3eca915b4
Phate334 <phate334@gmail.com>
2023-02-11 00:17:49 +0800

    [add] run all strategies.

---
+++ trader/base.py
@@ -0,0 +1,11 @@
+"""Base module for all strategies."""
+
+
+class BaseStrategy:
+    """Base class for all strategies."""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def do_backtest(self):
+        pass
---
+++ trader/run_strategies.py
@@ -0,0 +1,9 @@
+"""Run all strategies in the strategies folder."""
+import logging
+
+from strategies import enable_strategies
+
+if __name__ == "__main__":
+    for i, strategy in enumerate(enable_strategies):
+        logging.info("Running strategy %d of %d", i + 1, len(enable_strategies))
+        strategy.do_backtest()
